8e4e26013d69f1acc3de440b1ce0d8424d821e46
islvld <vladbatin@inbox.ru>
2023-04-26 00:46:14 +0500

    +HashF, stack and queue

---
+++ QUEUE/QUEUE.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+
+template <typename T>
+class Queue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
+    };
+
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = nullptr;
+        rear = nullptr;
+    }
+
+    ~Queue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+
+    void enqueue(T item) {
+        Node* newNode = new Node;
+        newNode->data = item;
+        newNode->next = nullptr;
+        newNode->prev = rear;
+
+        if (isEmpty()) {
+            front = newNode;
+        }
+        else {
+            rear->next = newNode;
+        }
+
+        rear = newNode;
+    }
+
+    T dequeue() {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        Node* temp = front;
+        T data = temp->data;
+
+        if (front == rear) {
+            front = nullptr;
+            rear = nullptr;
+        }
+        else {
+            front = front->next;
+            front->prev = nullptr;
+        }
+
+        delete temp;
+        return data;
+    }
+
+    T peek() {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        return front->data;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+};
+
+int main() {
+    Queue<int> queue;
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    std::cout << queue.peek() << std::endl;
+    queue.dequeue();
+    std::cout << queue.peek() << std::endl;
+    queue.dequeue();
+    std::cout << queue.peek() << std::endl;
+    queue.dequeue();
+    std::cout << queue.isEmpty() << std::endl;
+    return 0;
+}
+//Метод enqueue добавляет новый элемент в конец очереди.
+//Метод dequeue удаляет элемент из начала очереди и возвращает его значение.
+//Метод peek возвращает значение элемента в начале очереди, не удаляя его.
+//Метод isEmpty проверяет, пуста ли очередь.
---
+++ STACK.25/STACK.25.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+template <typename T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+
+    Node* top;
+
+public:
+    Stack() {
+        top = nullptr;
+    }
+
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(T item) {
+        Node* newNode = new Node;
+        newNode->data = item;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        Node* temp = top;
+        T data = temp->data;
+        top = top->next;
+        delete temp;
+        return data;
+    }
+
+    T peek() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        return top->data;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+};
+
+int main() {
+    Stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    std::cout << stack.peek() << std::endl;
+    stack.pop();
+    std::cout << stack.peek() << std::endl;
+    stack.pop();
+    std::cout << stack.peek() << std::endl;
+    stack.pop();
+    std::cout << stack.isEmpty() << std::endl;
+    return 0;
+}
+
+//В данной реализации стека с помощью односвязного списка создается класс Stack, содержащий приватное поле top, указывающее на вершину стека.
+//Внутренний класс Node используется для хранения данных элемента стека и указателя на следующий элемент.
+//Метод push добавляет новый элемент на вершину стека.
+//Метод pop удаляет элемент с вершины стека и возвращает его значение.Метод peek возвращает значение элемента на вершине стека, не удаляя его.
+//Метод isEmpty проверяет, пуст ли стек.
