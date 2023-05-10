6d787595afca13835f3658740f64dac726608d2c
islvld <vladbatin@inbox.ru>
2023-05-10 12:55:50 +0500

    upd

--- KL.11.25/KL.1.25/fraction.h
+++
@@ -1,9 +0,0 @@
-struct fraction
-{
-	int first;
-	int second;
-	void Init(int, int); // метод инициализации полей
-	void Read(); // метод для чтения значений полей
-	void Show(); // метод для вывода значений полей
-	int Minutes(); // приведение времени в минуты
-};
